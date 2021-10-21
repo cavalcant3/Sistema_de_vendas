b816b47ab6a978a509dbf044a1b232f8bcd4c993
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-21 10:29:26 -0300

    configurações de produtos

--- vendas-sm/vendas-sm/src/main/java/com/teste/aptidao/vendassm/produto/Produto.java
+++ vendas-sm/vendas-sm/src/main/java/com/teste/aptidao/vendassm/produto/Produto.java
@@ -1,4 +1,29 @@
 package com.teste.aptidao.vendassm.produto;
 
+import javax.persistence.*;
+
+@Entity
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Double preco;
+    private String nome;
+
+    @Deprecated
+    public Produto() {
+    }
+
+    public Produto(Double preco, String nome) {
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
