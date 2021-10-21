700fa3065c1292ea1197d42247749c19910de269
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-21 09:45:58 -0300

    Projeto-spring

---
+++ vendas-sm/vendas-sm/src/main/java/com/teste/aptidao/vendassm/VendasSmApplication.java
@@ -0,0 +1,13 @@
+package com.teste.aptidao.vendassm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasSmApplication.class, args);
+	}
+
+}
